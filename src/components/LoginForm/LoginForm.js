4ed0e668eb923b73fe18c1bcf0ce01476259f925
Adam Pavlicek <adamhpavlicek@gmail.com>
2019-07-03 14:30:38 -0500

    use hooks

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,20 +1,42 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import AuthApiService from '../../services/auth-api-service'
 import UserContext from '../../contexts/UserContext'
 import { Input, Label } from '../../components/FormUtils/FormUtils'
 import './LoginForm.css'
 
 export default function LoginForm() {
+  const [user, setUser] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handlesubmit = e => {
+    e.preventDefault()
+
+    AuthApiService.postLogin({
+      username: user,
+      password: password,
+    })
+      .then(res => {
+        user = ''
+        password = ''
+        
+      })
+      
+  }
+
   return (
     <div className="login">
       <div className="login-wrapper">
-        <form className="login-form">
+        <form
+          className="login-form"
+          onSubmit={handlesubmit}
+        >
           <div className="username-section">
             <Label className="username">Username</Label>
             <Input
               id="login-username-input"
               type="text"
               name="username"
+              onChange={e => setUser(e.target.value)}
               required
             />
           </div>
@@ -24,6 +46,7 @@ export default function LoginForm() {
               id="login-password-input"
               type="password"
               name="password"
+              onChange={e => setPassword(e.target.value)}
               required
             />
           </div>
