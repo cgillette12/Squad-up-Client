03ef2dbfe7ee3c2d0bcfefa649479875d0fcaf5f
Cody Gillette <cgillette1312@gmail.com>
2019-07-03 10:28:12 -0700

    updated login with functions

--- src/Routes/LoginRoute/LoginRoute.js
+++ src/Routes/LoginRoute/LoginRoute.js
@@ -3,19 +3,6 @@ import LoginForm from '../../components/LoginForm/LoginForm'
 
 
 export default function LoginRoute() {
-  // static defaultProps = {
-  //   location: {},
-  //   history: {
-  //     push: () => { },
-  //   },
-  // }
-  // handleLoginSuccess = () => {
-  //   const { location, history } = this.props
-  //   const destination = (location.state || {}).from || '/'
-  //   history.push(destination)
-  //   this.props.set();
-  // }
-
     return (
       <div className='login-parent'>
         <section className='login-section'>
