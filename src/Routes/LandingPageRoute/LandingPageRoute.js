994314f9ecd56a7e73ecc56c3fb22cac90cbd13f
Nick Lee <leexnick@gmail.com>
2019-07-03 12:01:33 -0700

    hook up landing page; move Landing-Page into src/components; write LandingPageRoute

---
+++ src/Routes/LandingPageRoute/LandingPageRoute.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import LandingPage from '../../components/Landing-Page/Landing-Page'
+
+export default function LandingPageRoute() {
+  return (
+    <LandingPage />
+  )
+}
