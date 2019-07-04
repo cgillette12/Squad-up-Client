ac02dd9fa0d7adccf633767c5b01059dc5e11b02
Cody Gillette <cgillette1312@gmail.com>
2019-07-04 11:48:41 -0700

    updated registration route

--- src/Routes/RegisterRoute/RegisterRoute.js
+++ src/Routes/RegisterRoute/RegisterRoute.js
@@ -1,12 +1,18 @@
 import React, { useState }from 'react'
 import RegisterForm from '../../components/RegisterForm/RegisterForm'
 
-export default function RegisterRoute() {
+export default function RegisterRoute(props) {
+   const handleRegisterSuccess = () => {
+    const {location = {}, history={push: () => {}}} = props;
+    const destination = (location.state || {}).from || '/'
+    history.push(destination)
+  }
+
   return (
     <div className='register-parent'>
       <section className='register-section'>
         <h1 className='register-Title'>Register</h1>
-        <RegisterForm />
+        <RegisterForm onRegisterSuccess={handleRegisterSuccess} />
       </section>
     </div>
   ) 
