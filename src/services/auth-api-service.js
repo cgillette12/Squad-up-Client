f5b37c0fb365c432cc4a81d029df07c8f3e78b47
Nick Lee <leexnick@gmail.com>
2019-07-06 10:25:33 -0700

    alter postUser body user_name to username; remove duplicate api in postUser fetch URL

--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -3,7 +3,7 @@ import TokenService from './token-service'
 
 const AuthApiService = {
   postUser(user) {
-    return fetch(`${config.API_ENDPOINT}/api/user`, {
+    return fetch(`${config.API_ENDPOINT}/user`, {
       method: 'POST',
       headers: {
         'content-type': 'application/json',
