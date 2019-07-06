6b54e8cfa355354a70cabece0d5032068558fc8a
Nick Lee <leexnick@gmail.com>
2019-07-06 14:20:24 -0700

    add fuzzy search

--- src/Routes/GamesListPage/GamesList.js
+++ src/Routes/GamesListPage/GamesList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext } from 'react'
 import GameContext from '../../contexts/GameContext'
 import GameApiService from '../../services/game-api.service'
 import GamesSublist from '../../components/GamesSublist/GamesSublist'
+import { Input } from '../../components/FormUtils/FormUtils'
 
 // import store from './dummy-store'
 
@@ -9,6 +10,7 @@ export default function GamesList() {
   const gameContext = useContext(GameContext)
   
   const [gamesList, setGamesList] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
   
   const gameTypes = [
     'Video Games',
@@ -32,21 +34,48 @@ export default function GamesList() {
 
   const renderGames = () => {
     return gameTypes.map((type, index) => {
-      return (
-        <GamesSublist
-          key={index}
-          type={type}
-          games={gamesList.filter(game => game.game_type === type)}
-        />
-      )
+      const gamesByType = gamesList.filter(game => game.game_type === type)
+
+      if(gamesByType.length !== 0) {
+        return (
+          <GamesSublist
+            key={index}
+            type={type}
+            games={gamesByType}
+          />
+        )
+      }
     })
   }
 
+  const handleSearchFuzzy = (input) => {
+    const filteredList = gameContext.gamesList.filter(game => game.game_title.includes(input))
+    setGamesList(filteredList)
+    setSearchTerm(input)
+  }
+
+  // const handleSearchSubmit = (e) => {
+  //   e.preventDefault()
+
+  //   setSearchTerm(e.target.value)
+  // }
+
   return (
     <section className="GamesList">
-      <form className="GamesList__search-bar">
-        <input type="text" placeholder="Search for Games..." name="search-games" />
-        <button type="submit">Submit</button>
+      <form 
+        className="GamesList__search-bar"
+        // onSubmit={e => handleSearchSubmit(e)}  
+      >
+        <Input 
+          type="text"
+          placeholder="Search for Games..."
+          name="search-games"
+          onChange={e => handleSearchFuzzy(e.target.value)}
+          value={searchTerm}
+          aria-label="Search for Games"
+          autoFocus
+        />
+        {/* <button type="submit">Search</button> */}
       </form>
       {renderGames()}
     </section>
