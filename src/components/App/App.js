43c65086eb25770f23cef368021404c52509dd34
Cody Gillette <cgillette1312@gmail.com>
2019-07-16 13:17:59 -0700

    updated nav/moble routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,10 @@ import { Route, Switch } from 'react-router-dom'
 import Header from '../Header/Header'
 import LoginRoute from '../../Routes/LoginRoute/LoginRoute'
 import LandingPageRoute from '../../Routes/LandingPageRoute/LandingPageRoute'
+import SquadList from '../../components/MySquads/MySquads'
+import GameList from '../../components/GamesList/GamesList'
 import RegisterRoute from '../../Routes/RegisterRoute/RegisterRoute'
+import { Redirect } from 'react-router'
 import NewSquadRoute from '../../Routes/NewSquadRoute/NewSquadRoute'
 import DashboardRoute from '../../Routes/DashboardRoute/DashboardRoute'
 import './App.css'
@@ -12,13 +15,16 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <main role="main">
+      <main role="main" query="(max-width:800px)">
        <Switch>
          <Route exact path={'/'} component={LandingPageRoute} />
          <Route exact path={'/login'} component={LoginRoute} />
+         <Route exact path={'/squads'} component={SquadList}/>
+         <Route exact path={'/games'} component={GameList} />
          <Route exact path={'/register'} component={RegisterRoute} />
          <Route exact path={'/newsquad'} component={NewSquadRoute}/>
          <Route path={'/dashboard'} component={DashboardRoute} />
+          <Redirect from="/" to="/games" />
        </Switch>
       </main>
     </div>
--- src/components/MySquads/MySquads.js
+++ src/components/MySquads/MySquads.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect, useContext } from 'react'
+import { Redirect } from 'react-router-dom'
 import SquadService from '../../services/Squad-api-service'
 import SquadContext from '../../contexts/SquadContext'
+import MobileUtils from '../../components/Utils/MobileUtils'
 import './MySquads.css';
 
 export default function MySquads() {
 
- 
+
   const [error, setError] = useState(null)
   const squadContext = useContext(SquadContext);
 
@@ -21,24 +23,24 @@ export default function MySquads() {
   }, [])
 
 
-
   return (
     <div className="Dashboard__user-squads">
       <div role='alert'>
         {error && <p>{error}</p>}
       </div>
+      <MobileUtils />
       <h3 className='My-squads'>My Squads</h3>
       <ul className='user-squads-list'>
         {squadContext.squadList.map((squadInfo, key) => {
           return (
             <li key={key} className='squad'>
-            <div className='squad-item-wrapper'>
-              <img id='squad-icon' src={squadInfo.userAvatar} alt=''/>
-              <p className='squad-name'>{squadInfo.squad_name}</p>
-            </div>
+              <div className='squad-item-wrapper'>
+                <img id='squad-icon' src={squadInfo.userAvatar} alt='' />
+                <p className='squad-name'>{squadInfo.squad_name}</p>
+              </div>
             </li>
           )
-          })}
+        })}
       </ul>
     </div>
   )
