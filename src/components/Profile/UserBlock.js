f3f1af5bd900572fd92d5ac02b598ce602998457
Harrison Chen <harrisonchen@harrisons-mbp.lan>
2019-07-15 22:05:22 -0700

    removed hover

--- src/components/Profile/UserBlock.js
+++ src/components/Profile/UserBlock.js
@@ -2,28 +2,28 @@ import React, { useRef, useState, useEffect } from 'react';
 import './UserBlock.css';
 
 
-function useHover(){
-    const ref =useRef()
-    const [hovered, setHovered] = useState(false)
+// function useHover(){
+//     const ref =useRef()
+//     const [hovered, setHovered] = useState(false)
 
-    const enter = () => setHovered(true)
-    const leave = () => setHovered(false)
+//     const enter = () => setHovered(true)
+//     const leave = () => setHovered(false)
 
-    useEffect( ()=> {
-        ref.current.addEventListener('mouseenter', enter)
-        ref.current.addEventListener('mouseleave', leave)
+//     useEffect( ()=> {
+//         ref.current.addEventListener('mouseenter', enter)
+//         ref.current.addEventListener('mouseleave', leave)
 
-        return () => {
-            ref.current.removeEventListener('mouseenter', enter)
-            ref.current.removeEventListener('mouseleave', leave)
-        }
-    }, [ref])
+//         return () => {
+//             ref.current.removeEventListener('mouseenter', enter)
+//             ref.current.removeEventListener('mouseleave', leave)
+//         }
+//     }, [ref])
 
-    return [ref, hovered]
-}
+//     return [ref, hovered]
+// }
 
 export default function UserBlock(props){
-    const [ref, hovered] = useHover()
+    // const [ref, hovered] = useHover()
     
     const ProcessBar = (xp, xpthreshold) => {
         const percent = xp/xpthreshold*100
@@ -41,29 +41,29 @@ export default function UserBlock(props){
             </div>
         )
     }
-    function renderAvatars(){
-        const avatars = [
-            "https://image.flaticon.com/icons/svg/78/78373.svg",
-            "https://image.flaticon.com/icons/svg/78/78373.svg",
-            "https://image.flaticon.com/icons/svg/78/78373.svg",
-            "https://image.flaticon.com/icons/svg/78/78373.svg",
-            "https://image.flaticon.com/icons/svg/78/78373.svg",
-            "https://image.flaticon.com/icons/svg/78/78373.svg"
-        ]
-        return avatars.map(image => {
-            return (
-                <div className="Avatar-Container" 
-                    style ={{backgroundImage :`${image}`}}
-                    key={image.charAt(15)}
-                    onClick={ev => {
-                        ev.preventDefault()
-                        props.setAvatar(image)
-                    }}
-                    >
-                </div>
-            )
-        })
-    }
+    // function renderAvatars(){
+    //     const avatars = [
+    //         "https://image.flaticon.com/icons/svg/78/78373.svg",
+    //         "https://image.flaticon.com/icons/svg/78/78373.svg",
+    //         "https://image.flaticon.com/icons/svg/78/78373.svg",
+    //         "https://image.flaticon.com/icons/svg/78/78373.svg",
+    //         "https://image.flaticon.com/icons/svg/78/78373.svg",
+    //         "https://image.flaticon.com/icons/svg/78/78373.svg"
+    //     ]
+    //     return avatars.map(image => {
+    //         return (
+    //             <div className="Avatar-Container" 
+    //                 style ={{backgroundImage :`${image}`}}
+    //                 key={image.charAt(15)}
+    //                 onClick={ev => {
+    //                     ev.preventDefault()
+    //                     props.setAvatar(image)
+    //                 }}
+    //                 >
+    //             </div>
+    //         )
+    //     })
+    // }
     return (
         <>
         <div className="User-Info-Contaiiner">
@@ -71,11 +71,11 @@ export default function UserBlock(props){
             <div 
                 className="User-Avatar-Container"
                 style={{ backgroundImage:`url(${props.avatar})`}}
-                ref={ref}
+                // ref={ref}
             >
-                {
+                {/* {
                     hovered ? <span>Change Avatar</span> : ""
-                }
+                } */}
             </div>
             <div className="Level-Container">
                 <h4>level:{props.level}</h4>
