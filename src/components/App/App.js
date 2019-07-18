e9d26e93628c3096a34c29f4fcc803f68b0500cc
Nick Lee <leexnick@gmail.com>
2019-07-17 19:46:27 -0700

    add paths for mobile view and clean up code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,28 +1,29 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Header from '../Header/Header'
-import LoginRoute from '../../Routes/LoginRoute/LoginRoute'
 import LandingPageRoute from '../../Routes/LandingPageRoute/LandingPageRoute'
-import SquadList from '../../components/MySquads/MySquads'
-import GameList from '../../components/GamesList/GamesList'
 import RegisterRoute from '../../Routes/RegisterRoute/RegisterRoute'
+import LoginRoute from '../../Routes/LoginRoute/LoginRoute'
+import SquadList from '../../components/MySquads/MySquads'
+import DashboardMain from '../DashboardMain/DashboardMain';
+import LiveChat from '../LiveChat/LiveChat'
 import DashboardRoute from '../../Routes/DashboardRoute/DashboardRoute'
 import './App.css'
-
 
 function App() {
   return (
     <div className="App">
       <Header />
       <main role="main">
-       <Switch>
-         <Route exact path={'/'} component={LandingPageRoute} />
-         <Route exact path={'/login'} component={LoginRoute} />
-         <Route exact path={'/squads'} component={SquadList}/>
-         <Route exact path={'/games'} component={GameList} />
-         <Route exact path={'/register'} component={RegisterRoute} />
-         <Route path={'/dashboard'} component={DashboardRoute} />
-       </Switch>
+        <Switch>
+          <Route exact path={'/'} component={LandingPageRoute} />
+          <Route exact path={'/register'} component={RegisterRoute} />
+          <Route exact path={'/login'} component={LoginRoute} />
+          <Route exact path={'/squads'} component={SquadList} />
+          <Route exact path={'/games'} component={DashboardMain} />
+          <Route exact path={'/chat'} component={LiveChat} />
+          <Route path={'/dashboard'} component={DashboardRoute} />
+        </Switch>
       </main>
     </div>
   )
