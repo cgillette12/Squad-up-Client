334333d66b119a8c217e96ed3cc4d05ba2c9dbc4
Nick Lee <leexnick@gmail.com>
2019-08-05 15:32:41 -0700

    fix mobile redirects for login & demo

--- src/Routes/LoginRoute/LoginRoute.js
+++ src/Routes/LoginRoute/LoginRoute.js
@@ -4,19 +4,23 @@ import { Redirect } from 'react-router-dom'
 import TokenService from '../../services/token-service'
 
 export default function LoginRoute(props) {
-
   const handleLoginSuccess = () => {
-    const {location = {}, history={push: () => {}}} = props;
-    console.log(window.innerWidth)
-    const destination = (location.state || {}).from || (window.innerWidth <= 800) ? '/games' : '/dashboard'
+    const { location = {}, history = { push: () => {} } } = props
+    // console.log(window.innerWidth)
+    const destination =
+      (location.state || {}).from || window.innerWidth <= 800
+        ? '/games'
+        : '/dashboard'
+    console.log(history)
+
     history.push(destination)
   }
 
   return (
-    <div className='login-parent'>
-      {TokenService.hasAuthToken()? <Redirect to="/dashboard"/> : <></>}
-      <section className='login-section'>
-        <h1 className='login-Title'>Login</h1>
+    <div className="login-parent">
+      {TokenService.hasAuthToken() ? <Redirect to="/dashboard" /> : <></>}
+      <section className="login-section">
+        <h1 className="login-Title">Login</h1>
         <LoginForm onLoginSuccess={handleLoginSuccess} />
       </section>
     </div>
