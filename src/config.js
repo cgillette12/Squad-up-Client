2e716a7c4aaf490bf37a2e7807497743326a1f50
Harrison Chen <harrisonchen@harrisons-mbp.lan>
2019-08-05 19:57:01 -0700

    fixed

--- src/config.js
+++ src/config.js
@@ -1,7 +1,5 @@
 export default {
-  // API_ENDPOINT: 'https://squadup-server.herokuapp.com/api',
-  // LIVE_CHAT_ENDPOINT: 'https://squadup-server.herokuapp.com',
-  API_ENDPOINT: 'http://localhost:8000/api',
-  LIVE_CHAT_ENDPOINT: 'http://localhost:8000',
+  API_ENDPOINT: 'https://squadup-server.herokuapp.com/api',
+  LIVE_CHAT_ENDPOINT: 'https://squadup-server.herokuapp.com',
   TOKEN_KEY: 'squad-up'
 }
