ed5b2929e2388aa4435de8166635df244c9e45a2
Nick Lee <leexnick@gmail.com>
2019-08-05 17:42:51 -0700

    add snapshot tests to DashboardMain

--- src/test/DashboardMain.test.js
+++ src/test/DashboardMain.test.js
@@ -1,13 +1,54 @@
-import React from 'react';
-import {BrowserRouter} from 'react-router-dom'
-import ReactDOM from 'react-dom';
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
 import DashboardMain from '../components/DashboardMain/DashboardMain'
 
-it('renders DashboardMain without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-    <BrowserRouter>
-    <DashboardMain />
-    </BrowserRouter>, div)
-  ReactDOM.unmountComponentAtNode(div)
+describe('Dashboard Main Component', () => {
+  it('renders DashboardMain without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(
+      <BrowserRouter>
+        <DashboardMain />
+      </BrowserRouter>,
+      div
+    )
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  it('renders the UI correctly', () => {
+    const createNodeMock = el => {
+      if (el.type === 'div') {
+        const mockEl = document.createElement('div')
+        mockEl.style.padding = '0'
+        Object.defineProperty(mockEl, 'clientHeight', {
+          get() {
+            return 100
+          },
+          enumerable: true,
+          configurable: true
+        })
+        Object.defineProperty(mockEl, 'clientWidth', {
+          get() {
+            return 300
+          },
+          enumerable: true,
+          configurable: true
+        })
+        return mockEl
+      }
+      return null
+    }
+
+    const tree = renderer
+      .create(
+        <BrowserRouter>
+          <DashboardMain />
+        </BrowserRouter>,
+        { createNodeMock }
+      )
+      .toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
 })
